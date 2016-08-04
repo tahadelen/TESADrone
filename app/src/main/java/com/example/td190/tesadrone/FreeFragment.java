9be6a6b42125429252ded34a7587940659274d36
tahadelen <tahadelen@gmail.com>
2016-08-04 14:41:13 +0300

    first commit

---
+++ app/src/main/java/com/example/td190/tesadrone/FreeFragment.java
@@ -0,0 +1,18 @@
+package com.example.td190.tesadrone;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FreeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.free_layout,null);
+    }
+}
---
+++ app/src/main/java/com/example/td190/tesadrone/SettingsFragment.java
@@ -0,0 +1,21 @@
+package com.example.td190.tesadrone;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ratan on 7/29/2015.
+ */
+public class SettingsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settings_layout,null);
+    }
+}
+
